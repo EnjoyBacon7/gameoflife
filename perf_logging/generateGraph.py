84f10358bca66640224d7aa76acac3963ee498ff
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-10-18 18:29:00 -0400

    Added data to generated graphs

--- perf_logging/generateGraph.py
+++ perf_logging/generateGraph.py
@@ -1,16 +1,26 @@
 import matplotlib.pyplot as plt
-import datetime
 import json
 import matplotlib as mpl
 
-
-def generateGraph(points, x_label, y_label, title, save_path):
+def generateGraph(points, x_label, y_label, title, save_path, data, dpi=100):
     plt.plot(points)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(title)
-    plt.savefig(save_path + "/" + title + ".png")
+
+    # Add corresponding parameters below the graph
+    plt.text(0.02, -0.2, "Resolution : " + str(data["resolution"]), fontsize=12, transform=plt.gca().transAxes)
+    plt.text(0.02, -0.275, "GUI : " + str(data["gui"]), fontsize=12, transform=plt.gca().transAxes)
+    plt.text(0.02, -0.35, "Log Type : " + str(data["log_type"]), fontsize=12, transform=plt.gca().transAxes)
+    plt.text(0.02, -0.425, "Log Name : " + str(data["log_name"]), fontsize=12, transform=plt.gca().transAxes)
+    plt.text(0.02, -0.5, "Speed : " + str(data["sim_speed"]), fontsize=12, transform=plt.gca().transAxes)
+
+    plt.subplots_adjust(bottom=0.3)
+
+    plt.savefig(save_path + "/" + title + ".png", dpi=dpi)
     plt.show()
+
+
 
 def loadData(log_path):
     data = []
@@ -24,9 +34,9 @@ def graphData(log_name):
     data  = loadData(log_path)
     points = []
     for entry in data["data"]:
-        for frame_time in entry["frame_times"]:
+        for frame_time in entry["frame_time"]:
             points.append(frame_time)
-    generateGraph(points, "Steps", "Frame Time (ns)",  data["log_type"] + " Performance " + log_name[:-12], "perf_logging/graphs")
+    generateGraph(points, "Steps", "Frame Time (ns)",  data["log_type"] + " Performance " + log_name[:-12], "perf_logging/graphs", data)
 
 if __name__ == "__main__":
 
@@ -36,6 +46,6 @@ if __name__ == "__main__":
     data = loadData(log_path)
     points = []
     for entry in data["data"]:
-        for frame_time in entry["frame_times"]:
+        for frame_time in entry["frame_time"]:
             points.append(frame_time)
-    generateGraph(points, "Steps", "Frame Time (ns)",  data["log_type"] + " Performance " + log_name[:-12], "graphs")
+    generateGraph(points, "Steps", "Frame Time (ns)",  data["log_type"] + " Performance " + log_name[:-12], "graphs", data)
