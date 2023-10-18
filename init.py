80b527341b139b87500ec67b5e312c39a0b5c767
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-10-18 15:28:37 -0400

    merged initPygame and initapp

--- init.py
+++ init.py
@@ -14,9 +14,8 @@ def initApp():
     group.add_argument("-lg", "--log_graphics", action="store_true", help="Log graphics data")
     group.add_argument("-ls", "--log_simulation", action="store_true", help="Log simulation data")
     parser.add_argument("-nogui", action="store_true", help="disable gui")
-    parser.add_argument("-r", "--resolution", nargs=2, type=int, help="set resolution")
+    parser.add_argument("-r", "--resolution", nargs=2, type=int, help="Set resolution")
     args = parser.parse_args()
-
 
     appState = {
         "resolution": None,
@@ -28,7 +27,7 @@ def initApp():
     if args.resolution:
         appState["resolution"] = args.resolution
     else:
-        appState["resolution"] = config.RESOLUTION
+        appState["resolution"] = (1080, 720)
 
     if args.nogui:
         appState["gui"] = False
@@ -41,14 +40,14 @@ def initApp():
         appState["logging"] = 2
     elif args.log_simulation:
         appState["logging"] = 3
+    else:
+        appState["logging"] = 0
 
-    appState["screen"] = initPygame(args.resolution)
+    pygame.init()
+    if(appState["logging"] == 0):
+        appState["screen"] = pygame.display.set_mode(args.resolution, pygame.RESIZABLE)
+    else:
+        appState["screen"] = pygame.display.set_mode(args.resolution)
+    pygame.display.set_caption("Game of Life")
 
     return appState
-
-# Initialise pygame
-def initPygame(resolution):
-    pygame.init()
-    screen = pygame.display.set_mode(resolution, pygame.RESIZABLE)
-    pygame.display.set_caption("Game of Life")
-    return screen
