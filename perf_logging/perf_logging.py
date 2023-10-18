e03cc6bd671da434fabf0cb660a85dfd0183443e
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-10-17 23:00:26 -0400

    Added logging to game from args

---
+++ perf_logging/perf_logging.py
@@ -0,0 +1,32 @@
+import datetime
+import json
+import generate_graph
+
+def init(log_state):
+    log_type = ""
+    if log_state == 0:
+        return
+    elif log_state == 1:
+        log_type = "full"
+    elif log_state == 2:
+        log_type = "graphics"
+    elif log_state == 3:
+        log_type = "simulation"
+
+    # Create file name
+    log_name = str(datetime.datetime.now()).replace(" ", "_").replace(":", "-") + ".json"
+    
+    log_data = {
+        "log_type": log_type,
+        "log_name": log_name,
+        "data": []
+    }
+
+    return log_data
+
+def dumpData(log_data):
+    log_name = log_data["log_name"]
+    with open("perf_logging/logs/" + log_name, "w") as f:
+        json.dump(log_data, f)
+    
+    generate_graph.main(log_name)
