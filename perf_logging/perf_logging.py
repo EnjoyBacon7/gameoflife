30e1eabb2e1867f1a89d018d591b5e2cef066f1e
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2023-10-18 11:19:03 -0400

    Merged generate_graph and perf_logging

--- perf_logging/perf_logging.py
+++ perf_logging/perf_logging.py
@@ -1,6 +1,7 @@
+import matplotlib.pyplot as plt
 import datetime
 import json
-import perf_logging.generate_graph as graph
+import matplotlib as mpl
 
 def init(log_state):
     log_type = ""
@@ -29,4 +30,27 @@ def dumpData(log_data):
     with open("perf_logging/logs/" + log_name, "w") as f:
         json.dump(log_data, f)
     
-    graph.main(log_name)
+    graph_data(log_name)
+
+def generateGraph(points, x_label, y_label, title):
+    plt.plot(points)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.savefig("perf_logging/graphs/" + title + ".png")
+    plt.show()
+
+def loadData(log_name):
+    data = []
+    with open("perf_logging/logs/" + log_name, 'r') as f:
+        data = json.load(f)
+    return data
+
+def graph_data(log_name):
+
+    data  = loadData(log_name)
+    points = []
+    for entry in data["data"]:
+        for frame_time in entry["frame_times"]:
+            points.append(frame_time)
+    generateGraph(points, "Steps", "Frame Time (ns)",  data["log_type"] + " Performance " + log_name[:-12])
